dcbac088e84b8328e352f6407d0413b753630a85
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2020-11-11 16:49:30 +0700

    admin

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -1,2 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+
 app = Flask(__name__)
+app.secret_key = 'kjif7346873249047287e&^#@&^%#'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/saledbv2?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app=app)
+admin = Admin(app=app,
+              name='IT81 SHOP 2',
+              template_mode='bootstrap4')
---
+++ saleapp/admin.py
@@ -0,0 +1,15 @@
+from saleapp import admin, db
+from flask_admin.contrib.sqla import ModelView
+from saleapp.models import Category, Product
+from flask_admin import BaseView, expose
+
+
+class ContactView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/contact.html')
+
+
+admin.add_view(ModelView(Category, db.session))
+admin.add_view(ModelView(Product, db.session))
+admin.add_view(ContactView(name='Liên hệ'))
